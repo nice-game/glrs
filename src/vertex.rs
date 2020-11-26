0982fb1e593e8cc1c1a3ebc4adbe6f9cae3cec62
Icy Defiance <icydefiance@gmail.com>
2020-11-25 23:31:35 -0500

    Remove mut requirement

--- src/vertex.rs
+++ src/vertex.rs
@@ -6,7 +6,11 @@ use crate::{
 use gl::types::{GLenum, GLint, GLuint};
 use nalgebra::{allocator::Allocator as NAllocator, DefaultAllocator, Dim, DimName, Quaternion, Scalar, Unit, VectorN};
 use simba::simd::SimdValue;
-use std::{mem::size_of, rc::Rc};
+use std::{
+	cell::{Cell, RefCell},
+	mem::size_of,
+	rc::Rc,
+};
 
 #[macro_export]
 macro_rules! implement_vertex {
@@ -95,46 +99,55 @@ pub struct VertexArray {
 	ctx: Rc<Ctx>,
 	handle: GLuint,
 	formats: Vec<Vec<VertexAttributeFormat>>,
-	next_attrib: GLuint,
-	element_buffer: Option<Rc<DynamicBuffer<[u8]>>>,
-	vertex_buffers: Vec<Option<Rc<DynamicBuffer<[u8]>>>>,
+	next_attrib: Cell<GLuint>,
+	element_buffer: RefCell<Option<Rc<DynamicBuffer<[u8]>>>>,
+	vertex_buffers: RefCell<Vec<Option<Rc<DynamicBuffer<[u8]>>>>>,
 }
 impl VertexArray {
 	pub fn new(ctx: &Rc<Ctx>) -> Self {
 		let mut handle = 0;
 		unsafe { ctx.gl.CreateVertexArrays(1, &mut handle) };
-		Self { ctx: ctx.clone(), handle, formats: vec![], next_attrib: 0, element_buffer: None, vertex_buffers: vec![] }
+		Self {
+			ctx: ctx.clone(),
+			handle,
+			formats: vec![],
+			next_attrib: 0.into(),
+			element_buffer: None.into(),
+			vertex_buffers: vec![],
+		}
 	}
 
-	pub fn enable_vertices<V: Vertex>(&mut self, divisor: GLuint) {
+	pub fn enable_vertices<V: Vertex>(&self, divisor: GLuint) {
 		let format = V::format();
 		let gl = &self.ctx.gl;
 		for &VertexAttributeFormat { offset, size, typ } in &format {
+			let next_attrib = self.next_attrib.get();
 			unsafe {
-				gl.EnableVertexArrayAttrib(self.handle, self.next_attrib);
-				gl.VertexArrayAttribFormat(self.handle, self.next_attrib, size, typ, gl::FALSE, offset);
-				gl.VertexArrayAttribBinding(self.handle, self.next_attrib, self.formats.len() as _);
+				gl.EnableVertexArrayAttrib(self.handle, next_attrib);
+				gl.VertexArrayAttribFormat(self.handle, next_attrib, size, typ, gl::FALSE, offset);
+				gl.VertexArrayAttribBinding(self.handle, next_attrib, self.formats.len() as _);
 				gl.VertexArrayBindingDivisor(self.handle, self.formats.len() as _, divisor);
 			}
-			self.next_attrib += 1;
+			self.next_attrib.set(next_attrib + 1);
 		}
 		self.formats.push(format);
-		self.vertex_buffers.push(None);
+
+		self.vertex_buffers.borrow_mut().push(None);
 	}
 
-	pub fn element_buffer(&mut self, element_buffer: &Allocator<u16>) {
+	pub fn element_buffer(&self, element_buffer: &Allocator<u16>) {
 		let element_buffer = element_buffer.buffer();
 		unsafe { self.ctx.gl.VertexArrayElementBuffer(self.handle, element_buffer.handle()) };
-		self.element_buffer = Some(element_buffer.clone());
+		*self.element_buffer.borrow_mut() = Some(element_buffer.clone());
 	}
 
-	pub fn vertex_buffer<V: Vertex>(&mut self, binding: usize, vertex_buffer: &Allocator<V>) {
+	pub fn vertex_buffer<V: Vertex>(&self, binding: usize, vertex_buffer: &Allocator<V>) {
 		assert_eq!(V::format(), self.formats[binding]);
 
 		let vertex_buffer = vertex_buffer.buffer();
 		let stride = size_of::<V>() as _;
 		unsafe { self.ctx.gl.VertexArrayVertexBuffer(self.handle, binding as _, vertex_buffer.handle(), 0, stride) };
-		self.vertex_buffers[binding] = Some(vertex_buffer.clone());
+		self.vertex_buffers.borrow_mut()[binding] = Some(vertex_buffer.clone());
 	}
 
 	pub fn handle(&self) -> GLuint {
